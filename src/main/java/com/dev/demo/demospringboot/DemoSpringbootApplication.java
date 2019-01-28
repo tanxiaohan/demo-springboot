4b438e2f16d200c4f9bbd8c4358b3d57e8bb09f3
tanxiaohan <tanxiaohan@dataeye.com>
2019-01-28 15:47:45 +0800

    new: 新建项目并引入web依赖

---
+++ src/main/java/com/dev/demo/demospringboot/DemoSpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.dev.demo.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootApplication.class, args);
+    }
+
+}
+
