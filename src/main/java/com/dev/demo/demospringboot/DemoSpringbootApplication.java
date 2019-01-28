5412954bed50775883e83e716fcbe546ed6d386a
tanxiaohan <tanxiaohan@dataeye.com>
2019-01-28 16:03:38 +0800

    new: 第一个REST API接口

--- src/main/java/com/dev/demo/demospringboot/DemoSpringbootApplication.java
+++ src/main/java/com/dev/demo/demospringboot/DemoSpringbootApplication.java
@@ -2,14 +2,24 @@ package com.dev.demo.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("hello")
 public class DemoSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringbootApplication.class, args);
     }
 
+
+    @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+    }
 }
 
